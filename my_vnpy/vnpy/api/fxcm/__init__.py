a18d975b51c8e22d7b54c3e1a01765aa2a94df5a
lj1918 <lj1918@sohu.com>
2018-10-14 22:00:20 +0800

    添加my_vnpy

---
+++ my_vnpy/vnpy/api/fxcm/__init__.py
@@ -0,0 +1,4 @@
+# encoding: UTF-8
+
+# from SmartTraderFrameWork.trader.api.fxcm.vnfxcm import  FxcmApi
+from .vnfxcm import FxcmApi
---
+++ my_vnpy/vnpy/trader/gateway/fxcmGateway/__init__.py
@@ -0,0 +1,5 @@
+#   作者 :             "刘军"
+#   创建日期：        2018/10/12
+
+from SmartTraderFrameWork.trader import stConstant
+from .fxcmGateway import F
